d0c74b8a06745755c70db798c219f7717cedfd3a
OscarMIP <oscar.mataro.epiaedu.cat>
2025-03-27 19:41:27 +0100

    refactor: restructure TodoApp components and styles, removing unused files and integrating new layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.module.css";
+import { Header } from "./Header";
+import { HeaderWrapper } from "./HeaderWrapper";
+import { ItemCard } from "./ItemCard";
+import { ListHeader } from "./ListHeader";
+import { Subheader } from "./Subheader";
+import { ListContainer } from "./ListContainer";
+import { ItemsList } from "./ItemsList";
+import { taskList } from "./taskList";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <HeaderWrapper> 
+        <Header title="TodoApp" />
+        <Subheader subtitle="Todo List Manager" />
+        <ListHeader content="Todo List" />
+      </HeaderWrapper>
+      <ListContainer>
+        <ItemsList itemsList={taskList}/>
+      </ListContainer>
+    </div>
+  ); 
 }
 
-export default App
+export default App;
+
+
+
